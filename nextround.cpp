600b994a1a62ee637c0c2a58366c6ffdea2023a9
niharikav27 <neerjav80@gmail.com>
2026-03-04 10:55:03 +0530

    brute force

---
+++ nextround.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,k;
+	cin >> n >> k;
+	int a[50];
+	for(int i = 0; i<n; i++){
+		cin >> a[i];
+	}
+	int score = a[k-1];
+	int count = 0;
+	for(int i = 0; i<n; i++){
+		if(a[i] >= score && a[i]>0){
+			count ++;
+		}
+	}
+    cout << count << endl;	
+}
