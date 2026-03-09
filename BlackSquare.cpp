60353283136eab04b281dcb980604c5d663765e4
niharikav27 <neerjav80@gmail.com>
2026-03-09 22:44:14 +0530

    Initial commit

---
+++ BlackSquare.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a1,a2,a3,a4;
+    cin>>a1>>a2>>a3>>a4;
+    string s;
+    cin>>s;
+    int total = 0;
+    for(int i = 0 ; i<s.length() ; i++){
+        if(s[i]=='1'){
+            total += a1;
+        }else if(s[i]=='2'){
+            total += a2;
+        }else if(s[i]=='3'){
+            total += a3;
+        }else if(s[i]=='4'){
+            total += a4;
+        }
+    }
+    cout<<total;
+    return 0 ;
+}
