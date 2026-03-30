1b5fc4138b5055e4e0db7ffecbfa50073c83d362
niharikav27 <neerjav80@gmail.com>
2026-03-30 13:06:20 +0530

    optimal

--- teamolympiad.cpp
+++ teamolympiad.cpp
@@ -1,37 +1,27 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
 int main(){
     int n;
-    cin>>n;
-    int t[n];
-    int c1 = 0 , c2 = 0 , c3 = 0;
-    for(int i =0;i<n;i++){
-        cin>>t[i]; 
-        if(t[i]==1){
-            c1++;
-        }
-        else if(t[i]==2){
-            c2++;
-        }
-        else if(t[i]==3){
-            c3++;
-        }
+    cin >> n;
+
+    vector<int> prog, math, pe;
+
+    for(int i = 1; i <= n; i++){
+        int x;
+        cin >> x;
+
+        if(x == 1) prog.push_back(i);
+        else if(x == 2) math.push_back(i);
+        else pe.push_back(i);
     }
-    int x = min(c1,min(c2,c3));
-    cout<<x;
-    int arr1,arr2,arr3;
-    for(int i=0;i<n;i++){
-        if(t[i]==1){
-            arr1+=i;
-        }
-        else if(t[i]==2){
-            arr2 +=i;
-        }
-        else if(t[i]==3){
-            arr3+=i;
-        }
+
+    int w = min({prog.size(), math.size(), pe.size()});
+    cout << w << endl;
+
+    for(int i = 0; i < w; i++){
+        cout << prog[i] << " " << math[i] << " " << pe[i] << endl;
     }
-   
-    
+
     return 0;
 }
