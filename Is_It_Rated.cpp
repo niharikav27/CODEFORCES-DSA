ff7268c3925e192b3c537998945b199a89d141b5
niharikav27 <neerjav80@gmail.com>
2026-04-18 19:51:57 +0530

    initial commit

---
+++ Is_It_Rated.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(a[i] != b[i]) {
+            cout << "rated";
+            return 0;
+        }
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        if(a[i] < a[i + 1]) {
+            cout << "unrated";
+            return 0;
+        }
+    }
+
+    cout << "maybe";
+}
